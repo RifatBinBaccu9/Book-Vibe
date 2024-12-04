7ae482ec38d8e1d27144bfb3d8d03bd3b86cbfb6
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2024-12-04 18:42:51 +0600

    dsa

--- src/Component/BookDetailsPage/BookDetailsPage.jsx
+++ src/Component/BookDetailsPage/BookDetailsPage.jsx
@@ -8,12 +8,12 @@ const BookDetailsPage = () => {
     const id=parseInt(bookId);
     const BookDetails =BookDetail.find(bookDetail=> bookDetail.bookId == id);
 
-    const {image,bookName,author,category,review,totalPages,yearOfPublishing,publisher,}=BookDetails;
+    const {image,bookName,author,category,review,totalPages,yearOfPublishing,publisher,tags,rating}=BookDetails;
     
     return (
-    <div className="flex flex-col md:flex-row gap-8 p-6 bg-gray-50 shadow-lg rounded-lg">
+    <div className=" w-[85%] mx-auto flex md:flex-row gap-8 p-6 rounded-lg">
       {/* Image Section */}
-      <div className="w-full md:w-1/3">
+      <div className="w-full md:w-1/2 bg-gray-50 rounded-lg p-14">
         <img
           src={image}
           alt={bookName}
@@ -25,45 +25,51 @@ const BookDetailsPage = () => {
       <div className="w-full md:w-2/3 space-y-4">
         {/* Book Name and Author */}
         <div>
-          <h1 className="text-2xl font-bold text-gray-800">{bookName}</h1>
-          <p className="text-md text-gray-600">by {author}</p>
+          <h1 className="text-4xl font-bold text-gray-800">{bookName}</h1>
+          <p className=" text-xl font-medium mt-3 text-gray-600">By: {author}</p>
         </div>
            <hr />
         {/* Category */}
-        <p className="text-sm text-gray-500">
+        <p className="text-md text-gray-500">
            {category}
         </p>
            <hr />
         {/* Review */}
         <p className="text-sm text-gray-500">
-          <span className="font-semibold">Review:</span> {review}
+          <span className="font-semibold text-lg">Review:</span> {review}
         </p>
-
+          
         {/* Tags */}
-        {/* <div>
-          <span className="font-semibold text-sm text-gray-500">Tags:</span>
+        <div className=' flex gap-3 mt-5'>
+          <span className="font-semibold text-sm text-gray-500 mt-1">Tags:</span>
           <div className="flex flex-wrap gap-2 mt-1">
-            {tags.map((tag, index) => (
+            {
+            tags.map((tag, idx) => (
               <span
-                key={index}
+                key={idx}
                 className="bg-blue-100 text-blue-600 text-xs px-3 py-1 rounded-full"
               >
                 {tag}
               </span>
             ))}
           </div>
-        </div> */}
+        </div>
+
+        <hr />
 
         {/* Total Pages, Publisher, and Year */}
         <div className="">
           <p className="text-sm text-gray-500">
-            <span className="font-semibold">Total Pages:{totalPages}</span> 
+            <span className="font-medium text-md">Total Pages:{totalPages}</span> 
+          </p>
+          <p className="text-sm text-gray-500 my-3">
+            <span className="font-medium text-lmd ">Publisher:{publisher}</span> 
           </p>
           <p className="text-sm text-gray-500">
-            <span className="font-semibold">Publisher:{publisher}</span> 
+            <span className="font-medium text-md">Year of Publishing: {yearOfPublishing}</span> 
           </p>
-          <p className="text-sm text-gray-500">
-            <span className="font-semibold">Year of Publishing: {yearOfPublishing}</span> 
+          <p className="text-sm text-gray-500 my-3">
+            <span className="font-medium text-md">rating : {rating}</span> 
           </p>
         </div>
 
